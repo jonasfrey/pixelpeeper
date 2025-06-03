3067578a58322b3c395ce1020363cb86835af838
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2025-06-03 16:32:40 +0200

    asdf

--- localhost/client.module.js
+++ localhost/client.module.js
@@ -197,12 +197,19 @@ let a_o_aspect_ratio =[
 ]
 let o_bcr_bgimg = {width:0};
 
+let a_n_u8_imagedata_overlay = new Uint8ClampedArray(0); // we dont need a proxy of this large array
 let a_n_u8_imagedata_original = new Uint8ClampedArray(0); // we dont need a proxy of this large array
 let a_n_u8_imagedata = new Uint8ClampedArray(0); // we dont need a proxy of this large array
 
-let a_n_y = new Uint8ClampedArray(0); // we dont need a proxy of this large array
+let a_n_y_to_take_from = new Uint8ClampedArray(0); // we dont need a proxy of this large array
+let a_n_y_to_start_coloring = new Uint8ClampedArray(0); // we dont need a proxy of this large array
+
+
 let o_state = f_o_proxified_and_add_listeners(
     {
+        b_lock_take_from_and_start_coloring: false,
+        b_take_from: true, 
+        b_start_coloring: false, 
         n_ms_timeout: 333,
         n_id_timeout: 0,
         s_src_img: '',
@@ -271,13 +278,11 @@ let f_update_canvas_timeout = function(
     // at HTMLCanvasElement.f_event_handler (module.js:2736:18)
     // fix the error by using the correct method to get the pixel data
 
-    for(let n_x2 = 0; n_x2 < a_n_y.length; n_x2++){
-        let n_y = a_n_y[n_x2];
-        if(n_y == 0){
-            continue;
-        }
+    for(let n_x2 = 0; n_x2 < a_n_y_to_start_coloring.length; n_x2++){
+        let n_y_take_from = a_n_y_to_take_from[n_x2];
+        let n_y_to_start_coloring = a_n_y_to_start_coloring[n_x2];
 
-        let n_idx1 = f_n_idx_pixel_from_n_x_n_y(n_x2, n_y, o_state.n_scl_x_px_image, o_state.n_scl_y_px_image, n_channels);
+        let n_idx1 = f_n_idx_pixel_from_n_x_n_y(n_x2, n_y_take_from, o_state.n_scl_x_px_image, o_state.n_scl_y_px_image, n_channels);
     
         let a_n_u8_imagedata_pixel = new Uint8ClampedArray(n_channels);
         a_n_u8_imagedata_pixel[0] = a_n_u8_imagedata_original[n_idx1]; // R
@@ -285,19 +290,38 @@ let f_update_canvas_timeout = function(
         a_n_u8_imagedata_pixel[2] = a_n_u8_imagedata_original[n_idx1+2]; // B            
         a_n_u8_imagedata_pixel[3] = a_n_u8_imagedata_original[n_idx1+3]; // A
 
-        for(let n_y2= n_y; n_y2 < o_state.n_scl_y_px_image; n_y2++){
-            // console.log('n_x2', n_x2, 'n_y2', n_y2)
+        if(n_y_take_from != -1){
             let n_idx_pixel = f_n_idx_pixel_from_n_x_n_y(
-                n_x2, n_y2, o_state.n_scl_x_px_image, o_state.n_scl_y_px_image, n_channels);
-            a_n_u8_imagedata[n_idx_pixel] = a_n_u8_imagedata_pixel[0]; // R
-            a_n_u8_imagedata[n_idx_pixel+1] = a_n_u8_imagedata_pixel[1]; // G
-            a_n_u8_imagedata[n_idx_pixel+2] = a_n_u8_imagedata_pixel[2]; // B
-            a_n_u8_imagedata[n_idx_pixel+3] = a_n_u8_imagedata_pixel[3]; // A
+                n_x2, n_y_take_from, o_state.n_scl_x_px_image, o_state.n_scl_y_px_image, n_channels);
+                a_n_u8_imagedata_overlay[n_idx_pixel] = 0; // R
+                a_n_u8_imagedata_overlay[n_idx_pixel+1] = 255
+                a_n_u8_imagedata_overlay[n_idx_pixel+2] = 0//a_n_u8_imagedata_pixel[2]; // B
+                a_n_u8_imagedata_overlay[n_idx_pixel+3] = 255//a_n_u8_imagedata_pixel[3]; // A
         }
+
+        if(n_y_to_start_coloring != -1){
+            for(let n_y2= n_y_to_start_coloring; n_y2 < o_state.n_scl_y_px_image; n_y2++){
+                // console.log('n_x2', n_x2, 'n_y2', n_y2)
+                let n_idx_pixel = f_n_idx_pixel_from_n_x_n_y(
+                    n_x2, n_y2, o_state.n_scl_x_px_image, o_state.n_scl_y_px_image, n_channels);
+                a_n_u8_imagedata[n_idx_pixel] = a_n_u8_imagedata_pixel[0]; // R
+                a_n_u8_imagedata[n_idx_pixel+1] = a_n_u8_imagedata_pixel[1]; // G
+                a_n_u8_imagedata[n_idx_pixel+2] = a_n_u8_imagedata_pixel[2]; // B
+                a_n_u8_imagedata[n_idx_pixel+3] = a_n_u8_imagedata_pixel[3]; // A
+                if(n_y2 ==n_y_to_start_coloring){
+                    a_n_u8_imagedata_overlay[n_idx_pixel] = 255; // R
+                    a_n_u8_imagedata_overlay[n_idx_pixel+1] = 0
+                    a_n_u8_imagedata_overlay[n_idx_pixel+2] = 0//a_n_u8_imagedata_overlay_pixel[2]; // B
+                    a_n_u8_imagedata_overlay[n_idx_pixel+3] = 255//a_n_u8_imagedata_pixel[3]; // A
+                }
+            }
+        }
+
+
     }
 
     //draw a_n_u8_imagedata to canvas 
-    let o_canvas = document.querySelector('canvas');
+    let o_canvas = document.querySelector('canvas#image');
     let o_ctx = o_canvas.getContext('2d');
     let o_image_data = new ImageData(
         a_n_u8_imagedata, 
@@ -305,54 +329,116 @@ let f_update_canvas_timeout = function(
         o_state.n_scl_y_px_image
     );
     o_ctx.putImageData(o_image_data, 0, 0);
+
+    let o_canvas2 = document.querySelector('canvas#overlay');
+    let o_ctx2 = o_canvas2.getContext('2d');
+    let o_image_data_overlay = new ImageData(
+        a_n_u8_imagedata_overlay, 
+        o_state.n_scl_x_px_image, 
+        o_state.n_scl_y_px_image
+    );
+    o_ctx2.putImageData(o_image_data_overlay, 0, 0);
+
+
     // f_o_toast('drawn pixel from image', 'info', 5000);
     // relative translation to the canvas element
 }
 
 let o = await f_o_html_from_o_js(
     {
-        class: "test",
-        style: "display: flex;flex-direction: row;",
         f_a_o: ()=>{
             return [
                 {
-                    s_tag: "canvas",
-                    onmousedown: function(o_event){
-                        if(o_event.button == 0){o_state.b_mouse_down_left = true;} 
-                        if(o_event.button == 1){o_state.b_mouse_down_middle = true;} 
-                        if(o_event.button == 2){o_state.b_mouse_down_right = true;} 
-                        let o_el = o_event.target;
-                        let o_rect = o_el.getBoundingClientRect();
-                        let o_trn_rel_to_el =
-                        {
-                            n_x: parseInt(o_event.clientX - o_rect.left),
-                            n_y: parseInt(o_event.clientY - o_rect.top)
-                        };
-                        // get the pixel of the mouse click
-                        console.log(o_trn_rel_to_el)
-
-                        a_n_y[o_trn_rel_to_el.n_x] = o_trn_rel_to_el.n_y;
-                        f_update_canvas();
-                        
-                    }, 
-                    onmousemove: function(o_event){
-                        if(o_state.b_mouse_down_left){
-                            let o_el = o_event.target;
-                            let o_rect = o_el.getBoundingClientRect();
-                            let o_trn_rel_to_el =
+                    class: "canvasses",
+                    style: 'position:relative',
+                    f_a_o:()=>{
+                        return [
                             {
-                                n_x: parseInt(o_event.clientX - o_rect.left),
-                                n_y: parseInt(o_event.clientY - o_rect.top)
-                            };
-                            console.log(o_trn_rel_to_el)
-
-
-                            a_n_y[o_trn_rel_to_el.n_x] = o_trn_rel_to_el.n_y;
-
-                            f_update_canvas();
-                        }
+                                style: 'position: absolute; top: 0; left: 0; z-index:1',
+                                s_tag: 'canvas',
+                                id: 'overlay', 
+                                onmousedown: (event)=>{
+                                    if(event.button == 0){o_state.b_mouse_down_left = true;}
+                                    if(event.button == 1){o_state.b_mouse_down_middle = true;}
+                                    if(event.button == 2){o_state.b_mouse_down_right = true;}
+                                },
+                                onmousemove: function(o_event){
+                                    if(o_state.b_mouse_down_left){
+                                        let o_el = o_event.target;
+                                        let o_rect = o_el.getBoundingClientRect();
+                                        let o_trn_rel_to_el = {
+                                            n_x: parseInt(o_event.clientX - o_rect.left),
+                                            n_y: parseInt(o_event.clientY - o_rect.top)
+                                        };
+                                        // console.log(o_trn_rel_to_el)
+                                        if(o_state.b_start_coloring){
+                                            a_n_y_to_start_coloring[o_trn_rel_to_el.n_x] = o_trn_rel_to_el.n_y;
+                                        }
+                                        if(o_state.b_take_from){
+                                            a_n_y_to_take_from[o_trn_rel_to_el.n_x] = o_trn_rel_to_el.n_y;
+                                        }
+                                        f_update_canvas();
+                                    }
+                                },
+                            },
+                            {
+                                style: 'position: absolute; top: 0; left: 0;',
+                                id: 'image', 
+                                s_tag: "canvas",
+                            },
+                        ]
+                    }
+                },
+                {
+                    s_tag: "button", 
+                    a_s_prop_sync: ['b_take_from','b_start_coloring', 'b_lock_take_from_and_start_coloring'],
+                    f_s_innerText: ()=>{
+                        return o_state.b_lock_take_from_and_start_coloring ? 'synced' : 'not synced';
                     },
-                    
+                    onclick: ()=>{
+                        o_state.b_lock_take_from_and_start_coloring = !o_state.b_lock_take_from_and_start_coloring;
+                        if(o_state.b_lock_take_from_and_start_coloring){
+                            o_state.b_start_coloring = true;
+                            o_state.b_take_from = true;
+                        }else{
+                            o_state.b_start_coloring = false;
+                            o_state.b_take_from = false;
+                        }
+                    }
+                },
+                {
+                    s_tag: "button", 
+                    a_s_prop_sync: ['b_take_from','b_start_coloring', 'b_lock_take_from_and_start_coloring'],
+                    f_s_innerText: ()=>{
+                        return `${(o_state.b_start_coloring) ? '[x]' : '[ ]'} start coloring`;
+                    },
+                    onclick: ()=>{
+                        if(o_state.b_lock_take_from_and_start_coloring){
+                            o_state.b_start_coloring = true;
+                            o_state.b_take_from = true;
+                        }
+                        else{
+                            o_state.b_start_coloring = true;
+                            o_state.b_take_from = false;
+                        }
+                    }
+                },
+                {
+                    s_tag: "button", 
+                    a_s_prop_sync: ['b_take_from','b_start_coloring', 'b_lock_take_from_and_start_coloring'],
+                    f_s_innerText: ()=>{
+                        return `${(o_state.b_take_from) ? '[x]' : '[ ]'} take from`;
+                    },
+                    onclick: ()=>{
+                        if(o_state.b_lock_take_from_and_start_coloring){
+                            o_state.b_start_coloring = true;
+                            o_state.b_take_from = true;
+                        }
+                        else{
+                            o_state.b_start_coloring = false;
+                            o_state.b_take_from = true;
+                        }
+                    }
                 },
                 {
                     s_tag: "input",
@@ -373,9 +459,13 @@ let o = await f_o_html_from_o_js(
                                     o_state.n_scl_y_px_image = o_img.height;
                                     o_state.b_loading_image = false;
                                     // draw image to canvas 
-                                    let o_canvas = document.querySelector('canvas');
+                                    let o_canvas = document.querySelector('canvas#image');
                                     o_canvas.width = o_img.width;
                                     o_canvas.height = o_img.height;
+                                    let o_canvas_overlay = document.querySelector('canvas#overlay');
+                                    o_canvas_overlay.width = o_img.width;
+                                    o_canvas_overlay.height = o_img.height;
+
                                     let o_ctx = o_canvas.getContext('2d');
                                     o_ctx.clearRect(0, 0, o_canvas.width, o_canvas.height);
                                     o_ctx.drawImage(o_img, 0, 0, o_img.width, o_img.height);
@@ -387,11 +477,21 @@ let o = await f_o_html_from_o_js(
                                         o_state.n_scl_y_px_image = o_img.height * n_scl_factor;
                                         o_canvas.width = o_state.n_scl_x_px_image;
                                         o_canvas.height = o_state.n_scl_y_px_image;
+                                        o_canvas_overlay.width = o_state.n_scl_x_px_image;
+                                        o_canvas_overlay.height = o_state.n_scl_y_px_image;
+                                        
+
                                         o_ctx.drawImage(o_img, 0, 0, o_state.n_scl_x_px_image, o_state.n_scl_y_px_image);
                                     }
                                     a_n_u8_imagedata_original = o_ctx.getImageData(0, 0, o_canvas.width, o_canvas.height).data;
                                     a_n_u8_imagedata = o_ctx.getImageData(0, 0, o_canvas.width, o_canvas.height).data;
-                                    a_n_y = new Array(o_canvas.width).fill(0);
+                                    a_n_u8_imagedata_overlay = new Uint8ClampedArray(a_n_u8_imagedata_original.length);
+                                    a_n_y_to_take_from = new Array(o_canvas.width).fill(-1);
+                                    a_n_y_to_start_coloring = new Array(o_canvas.width).fill(-1);
+                                    document.querySelector('.canvasses').style.width = `${o_canvas.width}px`;
+                                    document.querySelector('.canvasses').style.height = `${o_canvas.height}px`;
+                                    globalThis.a_n_y_to_take_from = a_n_y_to_take_from
+                                    globalThis.a_n_y_to_start_coloring = a_n_y_to_start_coloring
                                 };
                                 o_img.src = s_dataurl_image;
                                 // o_state.b_loading_image = true
